3a21dbc45301968d90d42c549ae04759ae24d217
viperguynaz <viperguynaz@gmail.com>
2022-02-24 15:18:08 -0700

    refactor sign-in for older APIs/devices

--- app/src/main/java/dev/zenrg/zenphotoframe/MainActivity.kt
+++ app/src/main/java/dev/zenrg/zenphotoframe/MainActivity.kt
@@ -2,33 +2,146 @@ package dev.zenrg.zenphotoframe
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.GoogleAuthException
+import com.google.android.gms.auth.GoogleAuthUtil
+import com.google.android.gms.auth.UserRecoverableAuthException
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.tasks.Task
 import dev.zenrg.zenphotoframe.models.MediaItem
 import dev.zenrg.zenphotoframe.viewmodel.MainViewModel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import java.io.IOException
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signInClient: GoogleSignInClient
     private val viewModel: MainViewModel by viewModels()
     private val tag = "zenx"
+    private val scope = "oauth2:https://www.googleapis.com/auth/photoslibrary.readonly"
+    private val job = Job()
+    private val scopeIO = CoroutineScope(job + Dispatchers.IO)
+
+    private var googleAccount: GoogleSignInAccount? = null
+
+    private var signInActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+        }
+    }
 
     private var mediaItems = mutableListOf<MediaItem>()
     private var token: String? by Delegates.observable(null) { property, oldValue, newValue ->
         Log.d(tag, "observable ${property.name}: $oldValue -> $newValue")
-        newValue?.let { viewModel.getMediaItems() }
+        newValue?.let { viewModel.setToken(it) }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.setToken(this)
+
+        //TODO - look at refactoring sign-in to reactive process with observables
+        // Build a GoogleSignInClient with the options specified by gso to refresh tokens
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.client_id_web))
+            .requestScopes(Scope("https://www.googleapis.com/auth/photoslibrary.readonly"))
+            .build()
+        signInClient = GoogleSignIn.getClient(this, gso)
+        googleAccount = GoogleSignIn.getAccountForScopes(this, Scope(scope))
+        when {
+            googleAccount == null -> {
+                silentSignIn()
+            }
+            googleAccount!!.isExpired -> {
+                signInActivity.launch(signInClient.signInIntent)
+            }
+            else -> {
+                setAccount(googleAccount)
+                Log.d(tag, "GoogleSignIn.getAccountForScopes email: ${googleAccount!!.email}")
+            }
+        }
+
+        // setup viewModel observers
         viewModel.mediaItemsLive.observe(this) { items ->
             items?.let {
                 mediaItems = it
             }
         }
         viewModel.authToken.observe(this) {
-            token = it
+            Log.d(tag, "viewModel.authToken set -- token: $it}")
+            viewModel.getMediaItems()
         }
+    }
+
+    private fun silentSignIn() {
+        val task: Task<GoogleSignInAccount> = signInClient.silentSignIn()
+        if (task.isSuccessful) {
+            setAccount(task.result)
+        } else {
+            task.addOnCompleteListener {
+                setAccount(it.result)
+            }
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private fun setAccount(signedInAccount: GoogleSignInAccount?) {
+        try {
+            googleAccount = signedInAccount
+            if (googleAccount == null || googleAccount!!.isExpired) {
+                signInActivity.launch(signInClient.signInIntent)
+            } else {
+                scopeIO.launch {
+                    try {
+                        token = GoogleAuthUtil.getToken(this@MainActivity, googleAccount!!.account!!, scope)
+                        Log.d(tag, "setAccount token: $token")
+                    } catch (transientEx: IOException) {
+                        // network or server error, the call is expected to succeed if you try again later.
+                        // Don't attempt to call again immediately - the request is likely to
+                        // fail, you'll hit quotas or back-off.
+
+                    } catch (e: UserRecoverableAuthException) {
+                        // Recover -- trying to access GoogleAuthUtil with expired account - launch sign-in
+                        Log.d(tag, "${e.message!!} --- launching sign-in")
+                        signInActivity.launch(e.intent)
+                    } catch (authEx: GoogleAuthException) {
+                        // Failure. The call is not expected to ever succeed so it should not be retried.
+                        authEx.printStackTrace()
+                    } catch (e: Exception) {
+                        throw RuntimeException(e)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(tag, e.message.toString())
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            googleAccount = completedTask.getResult(ApiException::class.java)
+            setAccount(googleAccount)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(tag, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
--- app/src/main/java/dev/zenrg/zenphotoframe/viewmodel/MainViewModel.kt
+++ app/src/main/java/dev/zenrg/zenphotoframe/viewmodel/MainViewModel.kt
@@ -1,30 +1,18 @@
 package dev.zenrg.zenphotoframe.viewmodel
 
-import android.content.Context
-import android.util.Log
 import androidx.lifecycle.*
-import com.google.android.gms.auth.GoogleAuthUtil
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.api.Scope
 import dev.zenrg.zenphotoframe.repository.GooglePhotosRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val repo = GooglePhotosRepository()
-    private val scope = "oauth2:https://www.googleapis.com/auth/photoslibrary.readonly"
     private val _authToken = MutableLiveData<String>()
     val authToken: LiveData<String> = _authToken
     val mediaItemsLive = repo.mediaItemsLive
 
-    fun setToken(context: Context) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val googleAccount = GoogleSignIn.getAccountForScopes(context, Scope(scope))
-            val androidAccount = googleAccount.account!!
-            val token = GoogleAuthUtil.getToken(context, androidAccount, scope)
-            Log.d("zenx", "setToken token: $token")
-            _authToken.postValue(token)
-        }
+    fun setToken(token: String) {
+        _authToken.postValue(token)
     }
 
     fun getMediaItems() {
